7c79c9ca317180a7f32904bb884679d431d03a5b
Michael Coppeta <mcoppeta3@gatech.edu>
2020-12-22 03:16:32 -0500

    Password entry binded to user selected

--- src/main/ui/TitleScreen.java
+++ src/main/ui/TitleScreen.java
@@ -1,12 +1,17 @@
 package main.ui;
 
 import javafx.beans.property.ReadOnlyListWrapper;
+import javafx.beans.property.ReadOnlyObjectProperty;
+import javafx.beans.property.ReadOnlyObjectWrapper;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import main.resources.UserResources;
@@ -25,20 +30,40 @@ public class TitleScreen extends StackPane {
 
         Label title = new Label("Title");
 
-        VBox userSelection = new VBox(30);
+        //Pane to hold usernames and password entry
+        VBox userSelection = new VBox(10);
         userSelection.setAlignment(Pos.CENTER);
 
         ObservableList<User> users = UserResources.getUsers();
         ReadOnlyListWrapper<String> usernames = new ReadOnlyListWrapper<>();
         usernames.set(FXCollections.observableArrayList());
+        usernames.add("<new user>");
         for (User u : users) {
             usernames.add(u.getName());
         }
-        ComboBox combo = new ComboBox(usernames);
+        ComboBox<String> combo = new ComboBox(usernames);
         combo.getSelectionModel().selectFirst();
 
-        userSelection.getChildren().addAll(combo);
+        TextField password = new TextField();
+        password.setPromptText("Enter Password");
 
+        userSelection.getChildren().addAll(combo);
+        combo.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observableValue, String oldValue, String newValue) {
+                if (newValue.equals("<new user>")) {
+                    if (userSelection.getChildren().contains(password)) {
+                        userSelection.getChildren().remove(password);
+                    }
+                } else {
+                    if (!userSelection.getChildren().contains(password)) {
+                        userSelection.getChildren().add(password);
+                    }
+                }
+            }
+        });
+
+        //Back in initial pane
         Button temp = new Button("Next");
 
         pane.getChildren().addAll(blank, title, userSelection, temp);
