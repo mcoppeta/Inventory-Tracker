814ec45ee1564a475249a0ae636d5b603a7d78c2
Michael Coppeta <mcoppeta3@gatech.edu>
2020-12-24 00:12:34 -0500

    Adds items to sign up page

--- src/main/ui/SignupScreen.java
+++ src/main/ui/SignupScreen.java
@@ -1,10 +1,13 @@
 package main.ui;
 
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import org.w3c.dom.Text;
 
 /**
  * Creates a screen to set up a new profile
@@ -15,10 +18,33 @@ public class SignupScreen extends StackPane {
      * Creates the sign up screen
      */
     public SignupScreen() {
-        VBox mainPane = new VBox();
+        // Main pane to hold everything
+        VBox mainPane = new VBox(50);
+        mainPane.setAlignment(Pos.TOP_CENTER);
 
+        Label temp = new Label("");
 
-        HBox buttonPane = new HBox();
+        Label title = new Label("Create New Profile");
+
+        // Horizontal pane to enter username
+        HBox usernamePane = new HBox(15);
+        usernamePane.setAlignment(Pos.CENTER);
+
+        Label usernameLabel = new Label("Enter Username:");
+        TextField usernameField = new TextField();
+        usernamePane.getChildren().addAll(usernameLabel, usernameField);
+
+        // Horizontal pane to enter password
+        HBox passwordPane = new HBox(15);
+        passwordPane.setAlignment(Pos.CENTER);
+
+        Label passwordLabel = new Label("Enter Password:");
+        TextField passwordField = new TextField();
+        passwordPane.getChildren().addAll(passwordLabel, passwordField);
+
+        // Horizontal pane for button controls
+        HBox buttonPane = new HBox(15);
+        buttonPane.setAlignment(Pos.CENTER);
 
         Button backButton = new Button("Back");
         backButton.setOnAction(e -> {
@@ -26,9 +52,9 @@ public class SignupScreen extends StackPane {
         });
 
         Button createButton = new Button("Create Profile");
-
         buttonPane.getChildren().addAll(backButton, createButton);
-        mainPane.getChildren().add(buttonPane);
+
+        mainPane.getChildren().addAll(temp, title, usernamePane, passwordPane, buttonPane);
         this.getChildren().add(mainPane);
 }
 }
